55091059bfe813f882371a24297f7535ff8534ba
Schroedl, Carl A <cschroedl@usgs.gov>
2014-08-21 15:55:03 -0500

    Online edit.

--- src/main/java/gov/usgs/cida/pubs/filter/CORSFilter.java
+++ src/main/java/gov/usgs/cida/pubs/filter/CORSFilter.java
@@ -22,7 +22,7 @@ public class CORSFilter implements Filter {
         response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
 
         chain.doFilter(req, resp);
     }

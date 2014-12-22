de7b7cc577fc687b272b28595f72c906c13f35ed
drsteini <drsteini@usgs.gov>
2014-12-22 13:28:10 -0600

    PUBSTWO-937 - SPN Lists - Display

---
+++ src/main/java/gov/usgs/cida/pubs/dao/intfc/IMpListDao.java
@@ -0,0 +1,14 @@
+package gov.usgs.cida.pubs.dao.intfc;
+
+import gov.usgs.cida.pubs.domain.mp.MpList;
+
+public interface IMpListDao extends IDao<MpList> {
+
+    /** 
+     * Get a mpList by it's ipds_internal_id.
+     * @param ipdsId - ipds_internal_id of the object to retrieve. 
+     * @return the domain object.
+     */
+	MpList getByIpdsId(Integer ipdsId);
+
+}
---
+++ src/test/java/gov/usgs/cida/pubs/webservice/security/EndpointSecurityAdTest.java
@@ -0,0 +1,31 @@
+package gov.usgs.cida.pubs.webservice.security;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class EndpointSecurityAdTest extends EndpointSecurityAuthTest {
+
+    @Before
+    public void setup() {
+    	super.setup();
+    }
+
+	@Test
+    public void adAuthenticatedTest() throws Exception {
+		mockSetup();
+		when(mockAuthClient.getRolesByToken("a-token-string")).thenReturn(new ArrayList<String>());
+
+    	publicTest(httpHeaders, status().isOk());
+    	authenticatedTest(httpHeaders, status().isOk());
+    	pubsAuthorizedTestGetsDeletes(httpHeaders, status().isForbidden(), false);
+    	pubsAuthorizedTestPosts(httpHeaders, status().isForbidden(), false);
+    	pubsAuthorizedTestPuts(httpHeaders, status().isForbidden(), false);
+    	adAuthenticatedOrPubsAuthorizedTest(httpHeaders, status().isOk());
+    }
+
+}

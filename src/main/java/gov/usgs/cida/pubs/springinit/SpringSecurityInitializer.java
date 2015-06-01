98fa81fe1d2978a9072bc11939c0d8c72b17f650
David Steinich <drsteini@usgs.gov>
2015-06-01 14:19:37 -0500

    Place CORS filter ahead of security filter

--- src/main/java/gov/usgs/cida/pubs/springinit/SpringSecurityInitializer.java
+++ src/main/java/gov/usgs/cida/pubs/springinit/SpringSecurityInitializer.java
@@ -1,11 +1,23 @@
 package gov.usgs.cida.pubs.springinit;
 
+import gov.usgs.cida.pubs.filter.CORSFilter;
+
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContext;
+
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
 public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
 	
 	public SpringSecurityInitializer() {
+//		super(SpringConfig.class, SecurityConfig.class, BusServiceConfig.class);
 		super(SecurityConfig.class);
+	}
+	
+	@Override
+	public void beforeSpringSecurityFilterChain(ServletContext servletContext) {		
+		FilterRegistration corsFilter = servletContext.addFilter("corsFilter", CORSFilter.class);
+		corsFilter.addMappingForUrlPatterns(null, false, "/*");
 	}
 
 }

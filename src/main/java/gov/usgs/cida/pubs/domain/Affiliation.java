8be8fa7d52f7f078acf2dd33b780a93ebf3cf2fa
abramhall <abramhall@usgs.gov>
2016-08-18 09:28:59 -0500

    cyclic validation

--- src/main/java/gov/usgs/cida/pubs/domain/Affiliation.java
+++ src/main/java/gov/usgs/cida/pubs/domain/Affiliation.java
@@ -2,8 +2,6 @@ package gov.usgs.cida.pubs.domain;
 
 import gov.usgs.cida.pubs.dao.intfc.IDao;
 import gov.usgs.cida.pubs.domain.intfc.ILookup;
-import gov.usgs.cida.pubs.validation.constraint.DeleteChecks;
-import gov.usgs.cida.pubs.validation.constraint.NoChildren;
 import gov.usgs.cida.pubs.validation.constraint.UniqueKey;
 
 import javax.validation.constraints.NotNull;
@@ -18,7 +16,7 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 @Component
 @UniqueKey(message = "{affiliation.name.duplicate}")
-@NoChildren(groups = DeleteChecks.class)
+//@NoChildren(groups = DeleteChecks.class)
 @JsonPropertyOrder({"id", "text", "active", "usgs"})
 public abstract class Affiliation<D extends Affiliation<D>> extends BaseDomain<D> implements ILookup {
 

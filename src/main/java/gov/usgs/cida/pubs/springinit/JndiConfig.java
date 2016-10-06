0c5d48a610db557edf6cc795493b94c8a3de4988
abramhall <abramhall@usgs.gov>
2016-10-06 09:19:11 -0500

    merge hell

--- src/main/java/gov/usgs/cida/pubs/springinit/JndiConfig.java
+++ src/main/java/gov/usgs/cida/pubs/springinit/JndiConfig.java
@@ -10,10 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 @Configuration
-@EnableSwagger2
 public class JndiConfig {
 	private static final Logger LOG = LoggerFactory.getLogger(JndiConfig.class);
 	

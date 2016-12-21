01cd909d509f182d6c5d02619ad43dd4bd3717a9
abramhall <abramhall@usgs.gov>
2016-12-21 14:49:58 -0600

    DS1

--- src/main/java/gov/usgs/cida/pubs/springinit/SpringSecurityInitializer.java
+++ src/main/java/gov/usgs/cida/pubs/springinit/SpringSecurityInitializer.java
@@ -2,6 +2,6 @@ package gov.usgs.cida.pubs.springinit;
 
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
-public class SpringSecurityInitializer {// extends AbstractSecurityWebApplicationInitializer {
+public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
 
 }

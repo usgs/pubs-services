84821fc97279c8dd6e90b17c964ba73a05687bd1
Steinich, Dave R. <drsteini@usgs.gov>
2017-04-03 13:25:18 -0500

    Remove commented out code

--- src/main/java/gov/usgs/cida/pubs/springinit/JmsConfig.java
+++ src/main/java/gov/usgs/cida/pubs/springinit/JmsConfig.java
@@ -21,11 +21,6 @@ public class JmsConfig {
 	@Autowired
 	@Qualifier("ipdsMessageListener")
 	MessageListener ipdsMessageListener;
-//	@Autowired
-//	String costCenterQueueName;
-//	@Autowired
-//	@Qualifier("costCenterMessageListener")
-//	MessageListener costCenterListener;
 
 	@Bean
 	public ActiveMQConnectionFactory connectionFactory() throws NamingException {
@@ -36,19 +31,6 @@ public class JmsConfig {
 		amqFactory.setRedeliveryPolicy(redeliveryPolicy);
 		return amqFactory;
 	}
-
-//	@Bean
-//	public BrokerService broker() throws NamingException, Exception {
-//		//This will prevent the never ending logs from the BrokerService saying there is not enough Temporary Store space...
-//		BrokerService broker = new BrokerService();
-//		broker.addConnector(brokerURL());
-//		broker.setPersistent(false);
-//		SystemUsage systemUsage = broker.getSystemUsage();
-//		systemUsage.getStoreUsage().setLimit(1024 * 1024 * 8);
-//		systemUsage.getTempUsage().setLimit(1024 * 1024 * 8);
-//		broker.start();
-//		return broker;
-//	}
 
 	@Bean
 	public DefaultMessageListenerContainer mlc() throws NamingException {
@@ -61,18 +43,5 @@ public class JmsConfig {
 		mlc.setSessionTransacted(true);
 		return mlc;
 	}
-
-//	@Bean
-//	public DefaultMessageListenerContainer ccmlc() throws NamingException {
-//		DefaultMessageListenerContainer ccmlc = new DefaultMessageListenerContainer();
-//		ccmlc.setConcurrentConsumers(1);
-//		ccmlc.setMaxConcurrentConsumers(1);
-//		ccmlc.setConnectionFactory(connectionFactory());
-//		ccmlc.setDestinationName(costCenterQueueName);
-//		ccmlc.setMessageListener(costCenterListener);
-//		ccmlc.setRecoveryInterval(50000);
-//		ccmlc.setSessionTransacted(true);
-//		return ccmlc;
-//	}
 
 }

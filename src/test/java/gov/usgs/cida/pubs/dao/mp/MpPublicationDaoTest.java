f2607b43c4d82b5a9cfc4a0b00ab351952cd9717
abramhall <abramhall@usgs.gov>
2017-04-05 11:12:57 -0500

    PUBSTWO-1475

--- src/test/java/gov/usgs/cida/pubs/dao/mp/MpPublicationDaoTest.java
+++ src/test/java/gov/usgs/cida/pubs/dao/mp/MpPublicationDaoTest.java
@@ -1,10 +1,10 @@
 package gov.usgs.cida.pubs.dao.mp;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertFalse;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -25,7 +25,6 @@ import gov.usgs.cida.pubs.PubsConstants;
 import gov.usgs.cida.pubs.dao.PublicationDao;
 import gov.usgs.cida.pubs.domain.ProcessType;
 import gov.usgs.cida.pubs.domain.Publication;
-import gov.usgs.cida.pubs.domain.PublicationLink;
 import gov.usgs.cida.pubs.domain.PublicationSeries;
 import gov.usgs.cida.pubs.domain.PublicationSubtype;
 import gov.usgs.cida.pubs.domain.PublicationTest;
@@ -254,25 +253,6 @@ public class MpPublicationDaoTest extends BaseSpringTest {
 		//This one is only in Publication, so we shouldn't frind it
 		assertNull(MpPublication.getDao().getByIndexId("9"));
 	}
-
-	@Test
-	@DatabaseSetups({
-		@DatabaseSetup("classpath:/testData/publicationType.xml"),
-		@DatabaseSetup("classpath:/testData/publicationSubtype.xml"),
-		@DatabaseSetup("classpath:/testData/publicationSeries.xml"),
-		@DatabaseSetup("classpath:/testData/dataset.xml")
-	})
-	public void testGetByLinkType() {
-		Map<String, Object> filters = new HashMap<>();
-		filters.put(PublicationDao.LINK_TYPE, new String[] {"Abstract"});
-		List<MpPublication> pubs = MpPublication.getDao().getByMap(filters);
-		assertNotNull(pubs);
-		assertFalse(pubs.isEmpty());
-		assertFalse(pubs.get(0).getLinks().isEmpty());
-		PublicationLink<?> publicationLink = (PublicationLink<?>)pubs.get(0).getLinks().toArray()[0];
-		assertEquals("Abstract",publicationLink.getLinkType().getText());
-	}
-
 
 	public static void assertMpPub1(Publication<?> pub, String expectedLockUsername) {
 		assertTrue(pub instanceof MpPublication);

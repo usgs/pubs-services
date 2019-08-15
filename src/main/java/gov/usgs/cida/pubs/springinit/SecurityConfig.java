7186be4eabd4ea227499268d5cdfd8460f9490e6
mbucknell <mbucknell@usgs.gov>
2019-08-15 14:44:40 -0500

    Also need to include Cors Configuration

--- src/main/java/gov/usgs/cida/pubs/springinit/SecurityConfig.java
+++ src/main/java/gov/usgs/cida/pubs/springinit/SecurityConfig.java
@@ -15,6 +15,11 @@ import org.springframework.security.oauth2.provider.token.store.jwk.JwkTokenStor
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 import gov.usgs.cida.pubs.ConfigurationService;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableResourceServer
@@ -46,7 +51,7 @@ public class SecurityConfig extends ResourceServerConfigurerAdapter {
 				.authorizeRequests()
 					//anonymous (public)
 					.antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
-					.antMatchers("/lookup/**", "/publication/**", "/version", "/about/**").permitAll()
+					.antMatchers("/lists/**","/mppublications/**", "/lookup/**", "/publication/**", "/version", "/about/**").permitAll()
 					.antMatchers("/swagger-ui.html", "/webjars/**", "/swagger-resources/**", "/v2/api-docs", "/v3/api-docs").permitAll()
 					//authenticated
 					.antMatchers("/mppublications/*/preview*", "/auth/logout").fullyAuthenticated()
@@ -60,5 +65,15 @@ public class SecurityConfig extends ResourceServerConfigurerAdapter {
 			.and()
 				.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
 		;
+	}
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("*"));
+		configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
 }

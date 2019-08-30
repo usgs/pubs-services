ddbbb4ae5a49e93e00ef85c522f0e090dc4e5981
Orosz <eorosz@igsarmewltesom.gs.doi.net>
2019-08-30 13:08:29 -0500

    Changes for jira PUBSTWO-1646 (rest call for creating pub from sipp)  Added class SippMpPubRequest

---
+++ src/main/java/gov/usgs/cida/pubs/domain/sipp/SippMpPubRequest.java
@@ -0,0 +1,43 @@
+package gov.usgs.cida.pubs.domain.sipp;
+
+import javax.validation.constraints.Pattern;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import gov.usgs.cida.pubs.domain.ProcessType;
+
+public class SippMpPubRequest {
+	public static final String IPNUMBER_VALIDATION_REGEX = "^" + "IP-\\d{6}" + "$";
+	public static final String IPNUMBER_VALIDATION_MESS = "Invalid IPNumber specified ('${validatedValue}') must be 'IP-' followed by 6 digits [0-9].";
+
+	//TODO: generic routine to create the validation regex and message from the enum.
+	public static final String PROCESS_TYPE_VALIDATION_REGEX = "(?i)" + "^" + "(DISSEMINATION)|(SPN_PRODUCTION)" + "$";
+	public static final String PROCESS_TYPE_VALIDATION_MESS = "Unknown ProcessType specified ('${validatedValue}') must be one of: 'DISSEMINATION', 'SPN_PRODUCTION'";
+
+	@JsonProperty("ProcessType")
+	@Pattern(regexp=PROCESS_TYPE_VALIDATION_REGEX, message=PROCESS_TYPE_VALIDATION_MESS)
+	private String processType;
+
+	@JsonProperty("IPNumber")
+	@Pattern(regexp=IPNUMBER_VALIDATION_REGEX, message=IPNUMBER_VALIDATION_MESS)
+	private String ipNumber;
+
+	public String getProcessType() {
+		return processType;
+	}
+	public void setProcessType(String processType) {
+		this.processType = processType;
+	}
+
+	public String getIpNumber() {
+		return ipNumber;
+	}
+	public void setIpNumber(String ipNumber) {
+		this.ipNumber = ipNumber;
+	}
+	
+	public ProcessType getProcessTypeEnum() {
+	    return ProcessType.valueOf(processType.toUpperCase());	
+	}
+
+}
